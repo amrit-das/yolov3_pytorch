5d54ab109eec124b8bd5c005eb3556d731081d16
amrit-das <amritdas1998@gmail.com>
2019-03-07 22:44:41 +0530

    fixes

--- video_detector.py
+++ video_detector.py
@@ -30,16 +30,12 @@ def detect_image(img):
     ratio = min(img_size/img.size[0], img_size/img.size[1])
     imw = round(img.size[0]*ratio)
     imh = round(img.size[1]*ratio)
-    img_transforms=transforms.Compose([transforms.Resize((imh,imw)),
-         transforms.Pad((max(int((imh-imw)/2),0), 
-              max(int((imw-imh)/2),0), max(int((imh-imw)/2),0),
-              max(int((imw-imh)/2),0)), (128,128,128)),
-         transforms.ToTensor(),
-         ])
+    img_transforms=transforms.Compose([transforms.Resize((imh,imw)),transforms.Pad((max(int((imh-imw)/2),0), max(int((imw-imh)/2),0), max(int((imh-imw)/2),0),max(int((imw-imh)/2),0)), (128,128,128)),transforms.ToTensor(),])
     
     image_tensor = img_transforms(img).float()
     image_tensor = image_tensor.unsqueeze_(0)
     input_img = Variable(image_tensor.type(Tensor))
+    
     with torch.no_grad():
         detections = model(input_img)
         detections = utils.non_max_suppression(detections, 80, conf_thres, nms_thres)
